b115296564848599939d0ed3e76f2ba5e50d466f
Carson Barnstable <cwbarnstable2.0@gmail.com>
2023-12-29 22:09:32 -0700

    Refactored Auxiliary Intelligence file for Speed/Readability/Efficiency

--- src/AuxiliaryIntelligence.py
+++ src/AuxiliaryIntelligence.py
@@ -23,92 +23,63 @@ class AuxiliaryIntelligence:
         for __ in range(3):
             self.rotate_piece()
 
+        # iterating through all possible starting rotations
         for rotation_iteration in range(4):
             self.rotate_piece()
 
+            # iterating through all possible starting horizontal positions
             for x_coord in range(self.grid_size[0]):
-                pieces = []
-
-                for piece in self.initial_piece_rects:
-                    pieces.append(piece.move(x_coord * self.tile_size[0], 0))
+                # assembling newly chosen piece
+                pieces = [piece.move(x_coord * self.tile_size[0], 0) for piece in self.initial_piece_rects]
                 self.drop_piece(pieces)
 
                 for piece in pieces:
                     piece.move_ip(0, -1*self.tile_size[1])
 
+                # taking notes of valid new configurations
                 if not self.touches_sides(pieces):
-                    valid_mass = self.initial_mass + pieces
-                    self.valid_masses[rotation_iteration, x_coord] = valid_mass
+                    self.valid_masses[rotation_iteration, x_coord] = self.initial_mass + pieces
 
         return self.valid_masses
 
     def center_piece(self):
         prior_center = self.initial_piece_rects[0].topleft
-
         for rect in self.initial_piece_rects:
             rect.move_ip(-1*prior_center[0], -1*prior_center[1])
 
     def rotate_piece(self):
-        rot_matrix = (1, -1)
-
+        # rotates a piece around it's pre-determined top-left corner
         centre_point = self.initial_piece_rects[0].topleft
+        core_coords = [c_p/t_s for c_p, t_s in zip(centre_point, self.tile_size)]
 
-        core_coords = []
-        for dimension in range(len(centre_point)):
-            core_coords.append(centre_point[dimension] / self.tile_size[dimension])
-
+        rot = (1, -1)
+        # performing rotation
         for rect in self.initial_piece_rects:
-            relative_pos = []
-            for dim in range(2):
-                relative_pos.append((rect.topleft[dim] / self.tile_size[dim]) - core_coords[dim])
-
-            new_coords = []
-            for i in range(len(relative_pos)):
-                new_coords.append(int((relative_pos[(i + 1) % 2] * rot_matrix[i] + core_coords[i]) * self.tile_size[i]))
-
+            rel = [(rect.topleft[dim] / self.tile_size[dim]) - core_coords[dim] for dim in range(2)]
+            new_coords = [int((rel[(i+1) % 2] * rot[i] + core_coords[i]) * self.tile_size[i]) for i in range(len(rel))]
             rect.topleft = tuple(new_coords)
 
     def drop_piece(self, piece):
-        for row_repeat in range(self.grid_size[1]):
+        # moves given piece to the lowest valid position
+        while not self.touches_mass_or_bottom(piece):
             for rect in piece:
                 rect.move_ip(0, self.tile_size[1])
-            if self.touches_mass_or_bottom(piece):
-                break
 
     def touches_mass_or_bottom(self, piece):
-        does_touch = False
+        # checks to ensure a given piece configuration doesn't hit the lower mass (or bottom if no mass)
         bottom_height = self.top_left_corner[1] + self.tile_size[1]*self.grid_size[1]
 
         for self_rect in piece:
-
-            if self_rect.bottom > bottom_height:
-                does_touch = True
-
-            for mass_rect in self.initial_mass:
-                if self_rect.colliderect(mass_rect):
-                    does_touch = True
-
-            if self_rect.top < self.top_left_corner[1]:
-                does_touch = True
-
-        return does_touch
-
-    def snap_up_to_grid(self, piece):
-        for rect in piece:
-            rect.move_ip(0, -1*self.tile_size[1])
-            rect.move_ip((0, -1*(rect.top % self.tile_size[1])))
+            if self_rect.bottom > bottom_height or self_rect in self.initial_mass:
+                return True
+        return False
 
     def touches_sides(self, piece):
-        does_touch = False
+        # checks to ensure a given piece configuration doesn't hit the play grid edge
         left_edge = self.top_left_corner[0]
         right_edge = self.top_left_corner[0] + self.tile_size[0]*self.grid_size[0]
 
         for self_rect in piece:
-            if self_rect.left < left_edge or self_rect.right > right_edge:
-                does_touch = True
-
-            for mass_rect in self.initial_mass:
-                if self_rect.colliderect(mass_rect):
-                    does_touch = True
-
-        return does_touch
+            if self_rect.left < left_edge or self_rect.right > right_edge or self_rect in self.initial_mass:
+                return True
+        return False
